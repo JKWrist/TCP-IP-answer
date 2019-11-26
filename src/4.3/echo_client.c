1988ec3b5560b7fd1e42f35ef5f83c85efef80d4
xujunze97 <xu_junze97@163.com>
2019-11-26 09:03:09 +0800

    修改exho_client,c

--- src/4.3/echo_client.c
+++ src/4.3/echo_client.c
@@ -18,5 +18,40 @@ int main(int argc, char *argv[])
 	char message[BUF_SIZE];
 	int str_len;
 	struct sockaddr_in serv_addr;
+
+	if(argc != 3)
+	{
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+	
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if(-1 == sock)
+		error_handling("socket() error");
+	
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
+
+	if(-1 == connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)))
+		error_handling("connect() error");
+	else 
+		puts("Connected");
+	
+	while (1) 
+	{
+		fputs("Inputs message(Q to quit)", stdout);
+		fgets(message, BUF_SIZE, stdin);
 		
+		if(!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+			break;
+		
+		write(sock, message, BUF_SIZE - 1);
+		str_len = read(sock, message, BUF_SIZE - 1);
+		message[str_len] = 0;
+		printf("Message from server : %s", message);
+	}
+	close(sock);
+	return 0;
 }
